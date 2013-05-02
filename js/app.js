95613a32ae23f3bb6ba476e4f032e088af96eb57
Ben Hamner <ben@benhamner.com>
2013-05-02 10:26:19 -0700

    removed cruft

--- js/app.js
+++ js/app.js
@@ -2,8 +2,6 @@ var fireDiceApp = angular.module('fireDiceApp', ['firebase']).
     config(function($routeProvider) {
         $routeProvider.
             when('/', { controller: FireDiceCtrl, templateUrl: 'main.html'}).
-            when('/login', { controller: FireDiceCtrl, templateUrl: 'login.html'}).
-            when('/game', { controller: FireDiceCtrl, templateUrl: 'game.html'}).
             otherwise({ redirectTo: '/' });
     }).
     factory('dataService', function($window) {
